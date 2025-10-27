b5e78701ffd56165ab2a8af90a108b0d575f4380
the-unknown-code <hello@unk.digital>
2025-10-27 12:33:57 +0700

    first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<storyblok-wrapper url="home">
+		<template #default="{ story, refreshKey }">
+			<main :key="refreshKey" class="page-index layout-block">
+				<h1>{{ story.name }}</h1>
+				<common-a-link href="/test">
+					<p class="mono-large">Go To Test Page</p>
+				</common-a-link>
+				<p v-for="i in 100" :key="i">{{ i }}</p>
+			</main>
+		</template>
+	</storyblok-wrapper>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.page-index {
+	padding: var(--spacer-64) 0;
+}
+</style>
